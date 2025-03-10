94d734a9528a886ae91d82e61300079820ce26bc
Jessica Wang <jessiwang2000@gmail.com>
2025-03-10 00:16:09 -0700

    rebase on main with levenshtein dist, update primary_analysis

--- data_analysis/primary_analysis.py
+++ data_analysis/primary_analysis.py
@@ -60,6 +60,7 @@ if __name__ == "__main__":
                                         
         df['rouge_sim_scores'] = df['rouge_sim_scores'].apply(parse_rouge_scores)
         df['cos_sim_scores'] = df['cos_sim_scores'].apply(float)
+        df['levenshtein_distance'] = df['levenshtein_distance'].apply(float)
 
 
         # Apply parsing to each dictionary entry
